782dd0043c9e1e24e63066591667cf6e5e2bec84
Alicia Evans <unleash.alicia@gmail.com>
2019-01-21 18:33:47 -0800

    Shows checkboxes for genres in edit view

--- app/controllers/films_controller.rb
+++ app/controllers/films_controller.rb
@@ -40,6 +40,9 @@ class FilmsController < ApplicationController
   # PATCH/PUT /films/1
   # PATCH/PUT /films/1.json
   def update
+    params[:film][:genre] ||= []
+    @film.genres << Genre.where(:id => params[:film][:genre_attributes])
+
     respond_to do |format|
       if @film.update(film_params)
         format.html { redirect_to @film, notice: 'Film was successfully updated.' }
--- app/models/film.rb
+++ app/models/film.rb
@@ -1,3 +1,4 @@
 class Film < ApplicationRecord
   has_and_belongs_to_many :genre
+  accepts_nested_attributes_for :genre
 end
